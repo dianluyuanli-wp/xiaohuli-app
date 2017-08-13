82e27f8499bdbc7daabd713784525e3a7c68d336
dianluyuanli-wp <waterdoor@foxmail.com>
2017-08-12 23:14:29 -0700

    edition

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-app.use(favicon(resolve('../dist/favicon.ico')));
+app.use(favicon(resolve('dist/favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(api);
